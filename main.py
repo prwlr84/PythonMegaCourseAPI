e02cf4d1150ce20b5edfcfd9dd486ddc0b519782
Antal Bako <antal.bako@tradelink.co>
2024-04-08 15:33:45 +0200

    Fixed email sending

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from send_email import send_email
 load_dotenv()
 api_key = os.getenv('API_KEY')
 url = 'https://newsapi.org/v2/everything?q=tesla&' \
-    'from=2024-02-29&sortBy=publishedAt&apiKey=24001234ba4f4bfeaf484ce8137fe68b'
+    'from=2024-03-29&sortBy=publishedAt&apiKey=24001234ba4f4bfeaf484ce8137fe68b'
 
 req = requests.get(url)
 content = req.json()
--- send_email.py
+++ send_email.py
@@ -1,21 +1,18 @@
 import smtplib
 import ssl
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-
-def send_email(msg):
-    host = 'smtp.google.com'
+def send_email(text):
+    host = 'smtp.gmail.com'
     port = 465
-    user = os.getenv('EMAIL')
+
+    login = os.getenv('EMAIL')
     pw = os.getenv('PW')
+
     context = ssl.create_default_context()
     receiver = 'prowlersk8@gmail.com'
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(user, pw)
-        print('Logged In')
-        server.sendmail(user, receiver, msg)
-        print('Sent')
+        server.login(login, pw)
+        server.sendmail(login, receiver, text)
